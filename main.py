51ed82a7516deb0dee7d85bffe30a1487f1a33a8
Alejandro Serna <jaiderserna2016@hotmail.com>
2023-12-30 14:47:57 -0500

    Fixed: append index every time the app was open, new: restore previously added tasks when reopening the app

--- main.py
+++ main.py
@@ -26,10 +26,10 @@ class TodayView(ctk.CTkFrame):
 
         # Create essential variables for later use
         self.taskName = ctk.StringVar()
-        self.a = 0
 
-        # Create and print widgets
+        # Create and print widgets, restore added tasks when reopening the app
         self.createWidgets()
+        self.restoreTasks()
 
     def createWidgets(self):
         # Create add task container, its entry and add button
@@ -48,6 +48,24 @@ class TodayView(ctk.CTkFrame):
         # Print global container itself
         self.pack()
 
+    def restoreTasks(self):
+        # Read tasks file, enumerate them and close their file
+        self.tasksFile = open("tasks.txt", "r")
+        self.tasks = self.tasksFile.readlines()
+        self.tasksFile.close()
+
+        # Store total tasks number
+        self.tasksNumber = len(self.tasks)
+
+        for a in range(self.tasksNumber):
+            # Create previously added task container, checkbox and name
+            self.taskFrame = ctk.CTkFrame(self.allTasksFrame)
+            self.taskCheckbox = ctk.CTkCheckBox(self.taskFrame, text = self.tasks[a])
+
+            # Print those widgets
+            self.taskFrame.pack(fill = "x")
+            self.taskCheckbox.pack(side = "left")
+
     def addTask(self):
         # Open tasks database file and append whatever the user wrote on the entry when he presses the add button
         self.tasksDatabase = open("tasks.txt", "a")
@@ -59,15 +77,16 @@ class TodayView(ctk.CTkFrame):
         self.tasksDatabase = open("tasks.txt", "r")
         self.task = self.tasksDatabase.readlines()
 
-        # Create a container for that added task, its checkbox and task name
+        # Append added task container, checkbox and name
         self.taskFrame = ctk.CTkFrame(self.allTasksFrame)
-        self.task = ctk.CTkCheckBox(self.taskFrame, text = self.task[self.a])
+
+        a = len(self.task)
+        # Fix current index because it's zero based
+        a -= 1
+        self.task = ctk.CTkCheckBox(self.taskFrame, text = self.task[a])
 
         # Print previous widgets in the tasks container
         self.taskFrame.pack(fill = "x")
         self.task.pack(side = "left")
-
-        # Grab next task when the user presses the button again
-        self.a += 1
 
 App()
